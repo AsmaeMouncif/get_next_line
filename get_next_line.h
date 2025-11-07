8c2760459847a4d96ca28a6973a3672e4b34f93d
Asmae Mouncif <asmounci@c3r4p1.1337.ma>
2025-11-07 12:00:56 +0100

    first version

---
+++ get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 11:13:31 by asmounci          #+#    #+#             */
+/*   Updated: 2025/11/06 16:57:07 by asmounci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*extract_line(char *str);
+
+#endif
---
+++ get_next_line_utils.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/06 11:07:10 by asmounci          #+#    #+#             */
+/*   Updated: 2025/11/07 11:56:17 by asmounci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	if (s == NULL)
+		return (0);
+	i = 0;
+	while (s[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	if (s == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (c == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	int		i;
+	int		j;
+	char	*p;
+
+	if (s2 == NULL)
+		return (NULL);
+	if (s1 == NULL)
+	{
+		s1 = malloc(1);
+		if (s1 == NULL)
+			return (NULL);
+		s1[0] = '\0';
+	}
+	p = malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (p == NULL)
+		return (free(s1), NULL);
+	i = -1;
+	while (s1[++i] != '\0')
+		p[i] = s1[i];
+	j = 0;
+	while (s2[j] != '\0')
+		p[i++] = s2[j++];
+	p[i] = '\0';
+	return (free(s1), p);
+}
+
+char	*extract_line(char *str)
+{
+	int		i;
+	int		j;
+	char	*line;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	if (str[i] == '\n')
+		line = malloc(i + 2);
+	else
+		line = malloc(i + 1);
+	if (line == NULL)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		line[j] = str[j];
+		j++;
+	}
+	if (str[i] == '\n')
+		line[j++] = '\n';
+	line[j] = '\0';
+	return (line);
+}
+
+char	*extract_rest(char *str)
+{
+	int		i;
+	char	*rest;
+	int		len;
+	int		j;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0' && str[i] != '\n')
+		i++;
+	if (str[i] != '\n' || str[i + 1] == '\0')
+		return (free(str), NULL);
+	len = ft_strlen(str) - i - 1;
+	rest = malloc(len + 1);
+	if (rest == NULL)
+		return (free(str), NULL);
+	i++;
+	j = 0;
+	while (str[i] != '\0')
+		rest[j++] = str[i++];
+	rest[j] = '\0';
+	return (free(str), rest);
+}
+
+#include <stdio.h>
+int main()
+{
+	char *s = "Hello";
+	char *x = ft_strchr(s, '\n');
+	printf("%s", x);
+	return 0;
+}
