9b5a8a963e9ff231a260d26c944726fb1bd97ebd
Asmae Mouncif <asmounci@e1r5p11.1337.ma>
2025-11-09 16:29:09 +0100

    final version

--- get_next_line_bonus.c
+++ get_next_line_bonus.c
@@ -1,38 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/05 17:42:41 by asmounci          #+#    #+#             */
-/*   Updated: 2025/11/09 16:27:28 by asmounci         ###   ########.fr       */
+/*   Created: 2025/11/09 14:59:20 by asmounci          #+#    #+#             */
+/*   Updated: 2025/11/09 16:28:51 by asmounci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
 
 char	*get_next_line(int fd)
 {
 	char		*buffer;
 	ssize_t		bytes_read;
-	static char	*rest;
+	static char	*rest[MAX_FD];
 	char		*line;
 
 	bytes_read = 1;
 	buffer = malloc(BUFFER_SIZE + 1);
 	if (buffer == NULL || fd < 0 || BUFFER_SIZE <= 0)
 		return (free(buffer), NULL);
-	while (bytes_read > 0 && ft_strchr(rest, '\n') == NULL)
+	while (bytes_read > 0 && ft_strchr(rest[fd], '\n') == NULL)
 	{
 		bytes_read = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_read < 0)
-			return (free(rest), free(buffer), rest = NULL, NULL);
+			return (free(rest[fd]), free(buffer), rest[fd] = NULL, NULL);
 		buffer[bytes_read] = '\0';
-		rest = ft_strjoin(rest, buffer);
+		rest[fd] = ft_strjoin(rest[fd], buffer);
 	}
-	line = extract_line(rest);
-	rest = extract_rest(rest);
+	line = extract_line(rest[fd]);
+	rest[fd] = extract_rest(rest[fd]);
 	free(buffer);
 	return (line);
 }
---
+++ get_next_line_bonus.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/09 15:00:40 by asmounci          #+#    #+#             */
+/*   Updated: 2025/11/09 16:28:31 by asmounci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
+# endif
+
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+char	*extract_line(char *str);
+char	*extract_rest(char *str);
+char	*get_next_line(int fd);
+
+#endif
